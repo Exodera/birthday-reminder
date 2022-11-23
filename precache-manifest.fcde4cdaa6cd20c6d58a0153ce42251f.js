c576fbf142ea962c814f177f0ce7ed791ca1bfbd
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-11-23 20:40:00 +0200

    Updates

---
+++ precache-manifest.fcde4cdaa6cd20c6d58a0153ce42251f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2afcc475c6b591d935d72387f6098a2",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "3bc89d4ea03a22cd657e",
+    "url": "/birthday-reminder/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc89d4ea03a22cd657e",
+    "url": "/birthday-reminder/static/js/main.4872ab76.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
